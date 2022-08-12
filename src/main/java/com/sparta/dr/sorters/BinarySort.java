b1d84589e9f97306bbfdc22b4f0ec9bb147bafc8
Dovydas Rimkus <drdovy@hotmail.com>
2022-08-12 16:07:48 +0100

    removed a logger

--- src/main/java/com/sparta/dr/sorters/BinarySort.java
+++ src/main/java/com/sparta/dr/sorters/BinarySort.java
@@ -6,10 +6,8 @@ import com.sparta.dr.sorters.binaryTree.BinaryTree;
 import com.sparta.dr.sorters.binaryTree.BinaryTreeImpl;
 
 public class BinarySort implements GenericSorter {
-    public static Logging logging = Logging.setupLogger();
     @Override
     public int[] sortArray(int[] array) {
-        logging.logger.info("Binary Sort Has Started");
         BinaryTree tree = new BinaryTreeImpl(array[0]);
         for (int i = 1; i < array.length; i++) {
             tree.addElement(array[i]);
